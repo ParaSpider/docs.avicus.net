886734154f2f7936ac45b173d5047ff76562c4e7
IvyCode <me@ivycode.net>
2015-01-24 18:28:34 -0600

    conf

---
+++ conf.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+import sys, os
+
+source_suffix = '.rst'
+master_doc = 'index'
+project = u'Avicus YAML'
+copyright = u'2014, Avicus Contributors'
+version = '0.0.1'
+release = '0.0.1'
+exclude_patterns = ['_build']
+pygments_style = 'sphinx'
